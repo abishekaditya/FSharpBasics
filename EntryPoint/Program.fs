3cbf62abfbfa00c1e30b28b4c8dbce3f71dc25cf
Abishek Aditya <abishekaditya@outlook.com>
2017-05-25 23:58:57 +0530

    Refactoring and paving the way for future updates

---
+++ EntryPoint/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let main x y =
+    x + y
+
+[<EntryPoint>]
+let mine argv = 
+    let r = main 20 15
+    printfn "%i" r
+    Console.ReadLine() |> ignore
+    0
---
+++ UseDisposable/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let createDisposable name = 
+    printfn "Creating %s" name
+    { new IDisposable with member x.Dispose() = printfn "Disposing %s" name }
+
+let main = 
+    use obj = createDisposable "Hey"
+    for i in [1..2] do
+        use nested = createDisposable (sprintf "Inner %i" i)
+        printfn "Inner %i" i
+    printfn "Leaving Inner"
+    ()
+
+open System.IO
+
+let writeToFile file (str : string) =
+    use fs = new FileStream(file, FileMode.CreateNew, FileAccess.Write)
+    let array = str.ToCharArray()
+    let barr = Array.map (fun x -> byte x) array
+    fs.Write(barr, 0, barr.Length)
+    
+[<EntryPoint>]
+let run args =
+    let x = main |> ignore
+    printfn "Done"
+    writeToFile "D" "ABCDEFGH"
+    Console.ReadLine() |> ignore
+    0
