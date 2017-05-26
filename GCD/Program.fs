a08cd00f813faf42174e78e53b24784caa94e78e
Abishek Aditya <abishekaditya@outlook.com>
2017-05-26 22:48:11 +0530

    Loops and GCD

---
+++ GCD/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let rec gcd x y =
+    if y= 0 then x 
+    else gcd y (x % y)
+
+[<EntryPoint>]
+let main argv = 
+    gcd 1245 3455 |> printf "GCD of 1245 and 3455 is %A \n"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ Loop/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    for i = 0 to 6 do printf "%i\n" (i*i)
+    for i = 15 downto 5 do printf "%i " (pown i 2)
+    let mutable i = 2
+    printf "\n"
+    while i < 20 do
+        printf "%i" i
+        i <- i + 1
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
