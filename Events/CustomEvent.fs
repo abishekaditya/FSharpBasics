9de5fa1a7b93816cb53ec22b5d14b17e2c073f4c
Abishek Aditya <abishekaditya@outlook.com>
2017-06-03 03:18:17 +0530

    Wrapping my head around a little bit of F# Events

---
+++ Events/CustomEvent.fs
@@ -0,0 +1,27 @@
+﻿module CustomEvent
+
+
+type FanState =
+   | Off = 0
+   | Low = 1
+   | Med = 2
+   | High = 3
+
+type Fan() =
+    let toggleEvent = Event<_>()
+    let mutable value = FanState.Off
+    member val Name = "" with get, set 
+    
+    member x.Value 
+        with get() = value
+
+    member x.AddHandlers(r) = 
+        Event.add r toggleEvent.Publish
+
+    member x.Toggle() =
+        match value with
+        | FanState.Off -> value <- FanState.Low
+        | FanState.Low -> value <- FanState.Med
+        | FanState.Med -> value <- FanState.High
+        | _ -> value <- FanState.Off
+        toggleEvent.Trigger()
---
+++ Events/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open CustomEvent
+    
+let hallFan = Fan(Name = "Hall Fan")
+hallFan.AddHandlers((fun _ -> (printfn "%s speed changed to %A" hallFan.Name hallFan.Value )))
+
+[<EntryPoint>]
+let main argv = 
+    let mutable t = 0
+    let timer = new Timers.Timer(500.0)
+    timer.Elapsed.Add (fun ea -> printfn "%d" t; t <- t + 1)
+    timer.Start()
+    while t < 5 do ()
+    timer.Dispose()
+    printfn "Custom Handler"
+    hallFan.Toggle()
+    hallFan.Toggle()
+    hallFan.Toggle()
+    hallFan.Toggle()
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
